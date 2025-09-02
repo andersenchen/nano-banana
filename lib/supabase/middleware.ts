7554285e0f9ad5fba91c5a47b817bd39539dc575
Andy Chen <andersenychen@gmail.com>
2025-09-02 15:56:57 -0400

    Fixed issue where non-authed home page was not showing images

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -52,7 +52,8 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/image")
+    !request.nextUrl.pathname.startsWith("/image") &&
+    !request.nextUrl.pathname.startsWith("/api/images")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
