0d0069e4e8b547955ead2a43c767fe1efd861054
Andy Chen <andersenychen@gmail.com>
2025-09-27 11:43:09 -0400

    Fix warning

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["rkfjktuwdwktjobigjti.supabase.co", "127.0.0.1"],
   },
+  allowedDevOrigins: ["play.fullstory.com"],
 };
 
 export default nextConfig;
