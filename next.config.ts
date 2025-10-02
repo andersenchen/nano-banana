466f2d887f325a759cefca71f696ccf296c25088
Andy Chen <andersenychen@gmail.com>
2025-10-01 22:02:07 -0400

    fix warning

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["rkfjktuwdwktjobigjti.supabase.co", "127.0.0.1"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rkfjktuwdwktjobigjti.supabase.co",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+    ],
   },
   allowedDevOrigins: ["play.fullstory.com"],
 };
